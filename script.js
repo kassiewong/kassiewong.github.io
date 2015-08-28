b6b7c8abf47c486046f42db3db069777a9d8e87e
kassiewong <470198@swcta.net>
2015-08-28 00:22:06 -0700

    initial

---
+++ script.js
@@ -0,0 +1,134 @@
+var myApp = angular.module('workApp', []);
+myApp.controller('workCtrl', function ($scope) {
+    $scope.projects = [
+        {
+            img: "img/sq_responsive.png",
+            imgProject: "../img/responsive.png",
+            name: "Responsive Client Website",
+            date: "2015-01-01",
+//            May 20, 2015
+            tenents: "Content Knowledge, Teamwork & Collaboration, Work Ethic, Presentation",
+            description: "A project showcasing our client communication and responsive design skills.",
+            id: "responsive"
+  },
+        {
+            img: "img/sq_shakespeare.png",
+            imgProject: "../img/shakespeare.png",
+            name: "Shakespeare Website",
+            date: "2014-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "shakespeare"
+  },
+        {
+            img: "img/git.png",
+            imgProject: "../img/git.png",
+            name: "Girls in Tech",
+            date: "2014-01-01",
+            tenents: "Tenents",
+            description: "A conference dedicated to expose middle school girls to the IT field.",
+            id: "git"
+  },
+        {
+            img: "img/sq_christmas.png",
+            imgProject: "../img/christmas.png",
+            name: "Christmas Banner Animation",
+            date: "2014-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "christmas"
+  },
+        {
+            img: "img/sq_qr.jpg",
+            imgProject: "../img/qr.jpg",
+            name: "QR Code Project",
+            date: "2013-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "qr"
+        },
+        {
+            img: "img/sq_lrfh.jpg",
+            imgProject: "../img/lrfh.jpg",
+            name: "Little Red Flying Hood Animation",
+            date: "2013-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "lrfh"
+        },
+        {
+            img: "img/sq_physics.png",
+            imgProject: "../img/physics.png",
+            name: "Physics PBL",
+            date: "2015-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "physics"
+        },
+        {
+            img: "img/sq_hiphop.jpg",
+            imgProject: "../img/hiphop.jpg",
+            name: "Hip Hop vs IT Conference",
+            date: "2015-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "hiphop"
+        },
+        {
+            img: "img/sq_sketchbook.png",
+            imgProject: "../img/sketchbook.png",
+            name: "Sketchbook Project",
+            date: "2015-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "sketchbook"
+        },
+        {
+            img: "img/warhol.png",
+            imgProject: "img/warhol.png",
+            name: "Andy Warhol Project",
+            date: "2014-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "warhol"
+        },
+        {
+            img: "img/bb.png",
+            imgProject: "../img/bb.png",
+            name: "Bouncing Ball Animation",
+            date: "2013-01-01",
+            tenents: "Tenents",
+            description: "Description",
+            id: "bb"
+        }]
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
